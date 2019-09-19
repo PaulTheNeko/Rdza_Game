3ac7a0fa3e204b1c17cad0379628dcf6b88b3133
Paweł Nowiński <tygrysekpawelpiotr1@gmail.com>
2019-09-19 17:09:26 +0200

    Added sdl2 and main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    // inicjalizacja sdl2
+    let sdl = sdl2::init().unwrap();
+    /* inicjalizacja podsystemu video
+    Wziąłem to z tutorialu*/
+    let video_subsystem = sdl.video().unwrap();
+    /* Stworzenie okna gry, też z tutorialu */
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    /* Czytanie Event'ów*/
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    /* Głowna pętla, adnotacja 'main by móc ją potem zamknąć */
+    'main: loop {
+            // Reakcja na event'y
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+        
+
+    }
 }
